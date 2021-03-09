19cddfde5e527a0e76388e882b438a3db89ac6ce
hanwj <hanwj@test.com>
2021-03-09 11:41:18 +0800

    创建spring-cloud-eureka

--- cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -2,17 +2,16 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author Hanwj
- * @Date 2021/3/8 18:24
+ * @Date 2021/3/8 20:02
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
-
+@EnableEurekaServer//开启Eureka服务端
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
